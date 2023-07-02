d79d82fe04f1e8a782b1bfe29fee35484b40a5bd
haller33 <eliseu42lucena@gmail.com>
2023-07-02 10:20:39 -0300

    improve build and testing files .asm

--- build.sh
+++ build.sh
@@ -8,12 +8,17 @@ fasm ./fasm/source/linux/fasm.asm
 
 mv ./fasm/source/linux/fasm bin
 
+for i in $(find t -name '*.asm'); do
+
+    ./bin/fasm $i
+done
+
 if [ $# -eq 2 ]; then
 
     odin build src -out:sim8086.bin -o:speed -reloc-mode:static && echo "OK"
 
 elif [ $# -eq 1 ]; then
-
+    
     odin build src -debug -out:sim8086.bin && echo "OK"
 else
 
